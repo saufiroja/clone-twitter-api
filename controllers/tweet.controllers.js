cf7d50579dd8541a317e611ec2b73dde1f8b570d
saufiroja <saufiroja08@gmail.com>
2022-01-10 21:30:30 +0800

    add feat find all and delete

--- controllers/tweet.controllers.js
+++ controllers/tweet.controllers.js
@@ -2,7 +2,7 @@ const createError = require('http-errors');
 
 const { Tweet } = require('../database/models');
 
-// POST TWEET
+// POST TWEET USER
 exports.createTweet = async (req, res, next) => {
   try {
     const { id } = req.user;
@@ -20,6 +20,69 @@ exports.createTweet = async (req, res, next) => {
     return res.status(201).json({
       message: 'successfully create tweet',
       code: 201,
+      tweet,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// FIND ALL TWEET FROM USER
+exports.findAllTweetUser = async (req, res, next) => {
+  try {
+    const { id } = req.user;
+    if (!id) {
+      return next(createError(401, 'unauthorized'));
+    }
+
+    const tweet = await Tweet.findAll({ where: { userId: id } });
+    return res.status(200).json({
+      message: 'successfully find all tweet from user',
+      code: 200,
+      tweet,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// FIND ALL TWEET
+exports.findAllTweet = async (req, res, next) => {
+  try {
+    const { id } = req.user;
+    if (!id) {
+      return next(createError(401, 'unauthorized'));
+    }
+
+    const tweet = await Tweet.findAll();
+    return res.status(200).json({
+      message: 'successfully find all tweet',
+      code: 200,
+      tweet,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// DELET TWEET FROM USER
+exports.deleteTweet = async (req, res, next) => {
+  try {
+    const { id } = req.user;
+    const tweetId = req.params.id;
+    if (!id) {
+      return next(createError(401, 'unauthorized'));
+    }
+
+    if (!tweetId) {
+      return next(createError(404, 'tweet not found'));
+    }
+
+    const tweet = await Tweet.destroy({ where: { id: tweetId } });
+
+    return res.status(200).json({
+      message: 'successfully delete tweet from user',
+      code: 200,
       tweet,
     });
   } catch (error) {
