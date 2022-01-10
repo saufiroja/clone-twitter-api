a8e22b1b1d7e2fd702d8b98d8f86bc9afa9019c5
saufiroja <saufiroja08@gmail.com>
2022-01-10 21:30:06 +0800

    add feat find all and delete

--- routers/tweet.routers.js
+++ routers/tweet.routers.js
@@ -1,10 +1,19 @@
 const router = require('express').Router();
 
-const { createTweet } = require('../controllers/tweet.controllers');
+const {
+  createTweet,
+  findAllTweet,
+  findAllTweetUser,
+  deleteTweet,
+} = require('../controllers/tweet.controllers');
 const { veirfyUser } = require('../middlewares/jwt.middlewares');
 const { validate } = require('../middlewares/joi/joi.middlewares');
 const { tweetSchema } = require('../middlewares/joi/tweet.schema');
 
-router.post('/tweet', veirfyUser, validate(tweetSchema), createTweet);
+router.get('/tweets', veirfyUser, findAllTweet);
+
+router.post('/user/tweets', veirfyUser, validate(tweetSchema), createTweet);
+router.get('/user/tweets', veirfyUser, findAllTweetUser);
+router.delete('/user/tweets/:id', veirfyUser, deleteTweet);
 
 module.exports = router;
