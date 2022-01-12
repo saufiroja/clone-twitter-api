b976bcb320ee2af349e7e4f4d7a72cc82dcc5baf
saufiroja <saufiroja08@gmail.com>
2022-01-12 14:37:24 +0800

    change function

--- middlewares/jwt.middlewares.js
+++ middlewares/jwt.middlewares.js
@@ -7,24 +7,26 @@ const { JWT_PUBLIC_KEY, JWT_ALGORITHMS } = process.env;
 
 const veirfyUser = async (req, res, next) => {
   const { authorization } = req.headers;
-  const token = authorization.split(' ')[1];
-
-  if (!token) {
-    return next(createError(401, 'unauthorized'));
-  }
-
-  try {
-    const secret = readFileSync(JWT_PUBLIC_KEY, { encoding: 'utf-8' });
-    const result = jwt.verify(token, secret, { algorithms: JWT_ALGORITHMS });
-    req.user = result;
-  } catch (error) {
+  if (authorization) {
+    const token = authorization.split(' ')[1];
+    try {
+      const secret = readFileSync(JWT_PUBLIC_KEY, { encoding: 'utf-8' });
+      const result = jwt.verify(token, secret, { algorithms: JWT_ALGORITHMS });
+      req.user = result;
+      next();
+    } catch (error) {
+      return res.status(401).json({
+        message: 'unauthorized',
+        code: 401,
+        error,
+      });
+    }
+  } else {
     return res.status(401).json({
-      message: 'invalid token',
+      message: 'Authentication error. Token required',
       code: 401,
-      error,
     });
   }
-  return next();
 };
 
 module.exports = { veirfyUser };
