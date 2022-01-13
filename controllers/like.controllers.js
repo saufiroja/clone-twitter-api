f6f3598e533f0e2366cf562d2815126b987785af
saufiroja <saufiroja08@gmail.com>
2022-01-13 21:49:26 +0800

    chenge function like

--- controllers/like.controllers.js
+++ controllers/like.controllers.js
@@ -11,29 +11,32 @@ exports.createLike = async (req, res, next) => {
       return next(createError(401, 'unauthorized'));
     }
 
-    // const tweet = await Tweet.findOne({ where: { id: tweetId } });
-    if (!tweetId) {
+    const tweet = await Tweet.findByPk(tweetId);
+    if (!tweet) {
       return next(createError(404, 'tweet not found'));
     }
 
-    const like = await Like.findOne({
+    let like = await Like.findOne({
       where: { [Op.and]: [{ userId, tweetId }] },
     });
 
-    if (like) {
-      return next(createError(400, 'the tweet has been liked'));
+    if (!like) {
+      let newLike = await Like.create({
+        userId,
+        tweetId,
+      });
+      return res.status(201).json({
+        message: 'successfully like tweet',
+        code: 201,
+        like: newLike,
+      });
+    } else {
+      await like.destroy();
+      return res.status(200).json({
+        message: 'successfully unlike tweet',
+        code: 200,
+      });
     }
-
-    const newLike = await Like.create({
-      userId,
-      tweetId,
-    });
-
-    return res.status(201).json({
-      message: 'successfully like tweet',
-      code: 201,
-      like: newLike,
-    });
   } catch (error) {
     next(error);
   }
